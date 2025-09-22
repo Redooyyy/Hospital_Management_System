2c4c389d558fd2ececf4a83587d503fc262325f7
Redooyyy <rwarieal@gmail.com>
2025-09-22 23:00:14 +0600

    Added codeowner to require my approval

--- src/main/java/Database/GetFrom_DB.java
+++ src/main/java/Database/GetFrom_DB.java
@@ -1,10 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Database;
 
-import com.mysql.cj.protocol.Resultset;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
