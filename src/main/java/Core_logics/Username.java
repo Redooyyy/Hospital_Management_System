6e76680cb5d1f63aff60cdae652beb80e654afff
Reo <122115413+Redooyyy@users.noreply.github.com>
2025-09-24 17:28:31 +0600

    Update Username.java

--- src/main/java/Core_logics/Username.java
+++ src/main/java/Core_logics/Username.java
@@ -1,9 +1,4 @@
-git/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Core_logics;
-
 /**
  *
  * @author Reo
