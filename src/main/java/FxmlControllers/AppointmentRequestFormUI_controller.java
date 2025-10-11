61f71a0b7eca9cc0531aeea83a05c2c31d1420ad
Redooyyy <rwarieal@gmail.com>
2025-10-11 11:08:09 +0600

    added date picker in appointment form

--- src/main/java/FxmlControllers/AppointmentRequestFormUI_controller.java
+++ src/main/java/FxmlControllers/AppointmentRequestFormUI_controller.java
@@ -1,25 +1,28 @@
 package FxmlControllers;
 
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import java.io.IOException;
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ResourceBundle;
 
 public class AppointmentRequestFormUI_controller implements Initializable {
     @FXML
     private AnchorPane formPane;
     @FXML
-    private TextField dateOfBirth;
+    private Label dateOfBirth;
     @FXML
     private TextArea address;
     @FXML
     private ChoiceBox<String>bloodGroupChoicebox;
+    @FXML
+    private DatePicker datepicker;
 
     private final String[] bloodGroup = {"A(+ve)","A(-ve)","B(+ve)","B(-ve)","AB(+ve)","AB(-ve)","O(+ve)","O(-ve)" };
 
@@ -39,4 +42,11 @@ public class AppointmentRequestFormUI_controller implements Initializable {
         dateOfBirth.setStyle("-fx-text-fill: white;"+"-fx-background-color: #2A3A4B;"+"-fx-background-radius:18;");
         bloodGroupChoicebox.getItems().addAll(bloodGroup);
     }
+
+    public void formFillUp(ActionEvent event){
+        LocalDate myDate = datepicker.getValue();
+        String date = myDate.format(DateTimeFormatter.ofPattern("dd-MM-yyy"));
+        dateOfBirth.setText(date);
+    }
+
 }
